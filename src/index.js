de30cd0265d119883b75ea879439383c8a014496
Stefan <trfstephan@gmail.com>
2023-11-14 10:52:04 +0100

    subida del proyecto a producción

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Importamos los archivos con los que vamos a trabajar
+// 📌 
+// Archivo que carga las categorías
+import './cargarCategorias'
+// Archivo que abre la galeria, con todos sus elementos, cuando hacemos click sobre una categoría,
+import './eventoCategorias'
+// Contiene todos los eventos de la galeria una vez abierta: Cerrar, siguiente slide
+import './galeria/eventosGaleria'
